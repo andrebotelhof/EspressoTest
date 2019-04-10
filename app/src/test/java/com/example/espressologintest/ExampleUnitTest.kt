ea471d34f699236f3555f3a2ff195d4fcb4a61ed
Felipe Castilhos <felipecastilhos@gmail.com>
2019-04-09 22:16:51 -0300

    Adding unit tests

--- app/src/test/java/com/example/espressologintest/ExampleUnitTest.kt
+++ app/src/test/java/com/example/espressologintest/ExampleUnitTest.kt
@@ -3,15 +3,35 @@ package com.example.espressologintest
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Before
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
+
 class ExampleUnitTest {
+    private var exampleModel = ExampleModel()
+
+
+    @Before
+    fun before() {
+        exampleModel = ExampleModel()
+    }
+
     @Test
-    fun addition_isCorrect() {
+    fun `assert addiction was correct`() {
         assertEquals(4, 2 + 2)
     }
+
+    @Test
+    fun `assert the variable is true`() {
+        exampleModel.exampleBoolean = true
+        val bool = exampleModel.exampleBoolean == true
+        assertTrue(bool)
+    }
+
+    @Test
+    fun `assert variabe is false`() {
+        val bool = exampleModel.exampleBoolean == true
+        assertFalse(bool)
+    }
+
+    private data class ExampleModel(var exampleBoolean: Boolean? = null)
 }
